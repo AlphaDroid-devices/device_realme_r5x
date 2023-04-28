6f7f0e5ea74b417848f4737765536cf1f49a1b0d
Dyneteve <sound0020@gmail.com>
2023-04-28 08:40:34 +0000

    r5x: Enable AOSP surfaceflinger
    * AOSP SurfaceFlinger is smoother than CAF

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -188,6 +188,9 @@ include device/qcom/sepolicy_vndr-legacy-um/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 
+# SurfaceFlinger
+TARGET_USE_AOSP_SURFACEFLINGER := true
+
 # WiFi
 BOARD_WLAN_DEVICE := qcwcn
 BOARD_HOSTAPD_DRIVER := NL80211
